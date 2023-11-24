b7beeb9a724756482cefd8745b693b67bd1a7b34
OmarWheed <wheedomar88@gmail.com>
2023-11-24 21:08:10 +0200

    take input and validate data

--- lib/widgets/model_button_sheet.dart
+++ lib/widgets/model_button_sheet.dart
@@ -10,30 +10,64 @@ class AddNote extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Gap(
-              height: 30,
-            ),
-            CustomTextField(
-              hint: "omar",
-            ),
-            Gap(
-              height: 30,
-            ),
-            CustomTextField(
-              hint: "Contant",
-              maxLine: 5,
-            ),
-            Gap(
-              height: 30,
-            ),
-            MyButton(),
-            Gap(
-              height: 30,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          Gap(
+            height: 30,
+          ),
+          CustomTextField(
+            onSaved: (p0) => title = p0,
+            hint: "omar",
+          ),
+          Gap(
+            height: 30,
+          ),
+          CustomTextField(
+            hint: "Contant",
+            maxLine: 5,
+            onSaved: (p0) => subTitle = p0,
+          ),
+          Gap(
+            height: 30,
+          ),
+          MyButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          Gap(
+            height: 30,
+          ),
+        ],
       ),
     );
   }
